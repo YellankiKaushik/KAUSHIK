f1acf72ba200cdf248ba18b899017f3c78af5ee5
YellankiKaushik <137211350+YellankiKaushik@users.noreply.github.com>
2026-03-31 19:57:55 +0530

    updating
    Removed the project link from experiences data.

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -47,7 +47,7 @@ export const experiences = [
         ],
 
         projectLink:
-            "https://github.com/YellankiKaushik/GPT-Integration-Optimization",
+            "",
 
         linktype: "github",
 
